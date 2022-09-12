e1e4c76f0a415373e26b678900a7640ab51356b4
Dave Longley <dlongley@digitalbazaar.com>
2022-09-11 20:22:26 -0400

    Update test files.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,52 @@
-/*
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+/*!
+ * Copyright (c) 2016-2022 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
+import * as bedrock from '@bedrock/core';
+import {getServiceIdentities} from '@bedrock/app-identity';
+import {handlers} from '@bedrock/meter-http';
+import '@bedrock/ssm-mongodb';
+import '@bedrock/kms';
+import '@bedrock/https-agent';
+import '@bedrock/meter';
+import '@bedrock/meter-usage-reporter';
+import '@bedrock/server';
+import '@bedrock/kms-http';
+import '@bedrock/edv-storage';
+import '@bedrock/vc-delivery';
+import '@bedrock/vc-issuer';
 
-const bedrock = require('bedrock');
-require('bedrock-https-agent');
-require('bedrock-mongodb');
-require('bedrock-module-template-http');
+import {mockData} from './mocha/mock.data.js';
 
-require('bedrock-test');
+bedrock.events.on('bedrock.init', async () => {
+  /* Handlers need to be added before `bedrock.start` is called. These are
+  no-op handlers to enable meter usage without restriction */
+  handlers.setCreateHandler({
+    handler({meter} = {}) {
+      // use configured meter usage reporter as service ID for tests
+      const clientName = mockData.productIdMap.get(meter.product.id);
+      const serviceIdentites = getServiceIdentities();
+      const serviceIdentity = serviceIdentites.get(clientName);
+      if(!serviceIdentity) {
+        throw new Error(`Could not find identity "${clientName}".`);
+      }
+      meter.serviceId = serviceIdentity.id;
+      return {meter};
+    }
+  });
+  handlers.setUpdateHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setRemoveHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setUseHandler({handler: ({meter} = {}) => ({meter})});
+});
+
+// mock oauth2 authz server routes
+bedrock.events.on('bedrock-express.configure.routes', app => {
+  app.get(mockData.oauth2IssuerConfigRoute, (req, res) => {
+    res.json(mockData.oauth2Config);
+  });
+  app.get('/oauth2/jwks', (req, res) => {
+    res.json(mockData.jwks);
+  });
+});
+
+import '@bedrock/test';
 bedrock.start();
