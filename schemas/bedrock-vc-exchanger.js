8fa291832412309abb9ae230e13bb41b49bd4ba5
Dave Longley <dlongley@digitalbazaar.com>
2022-10-30 16:54:26 -0400

    Generate and check access token from per-exchange-instance AS.

--- schemas/bedrock-vc-exchanger.js
+++ schemas/bedrock-vc-exchanger.js
@@ -1,6 +1,41 @@
 /*!
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
+const oidc4vciExchangeOptions = {
+  title: 'OIDC4VCI Exchange options',
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    preAuthorizedCode: {
+      type: 'string'
+    },
+    oauth2: {
+      title: 'OIDC4VCI Exchange OAuth2 Options',
+      type: 'object',
+      additionalProperties: false,
+      required: ['keyPair'],
+      properties: {
+        keyPair: {
+          type: 'object',
+          additionalProperties: false,
+          required: ['privateKeyJwk', 'publicKeyJwk'],
+          properties: {
+            privateKeyJwk: {
+              type: 'object'
+            },
+            publicKeyJwk: {
+              type: 'object'
+            }
+          }
+        },
+        maxClockSkew: {
+          type: 'number'
+        }
+      }
+    }
+  }
+};
+
 export const createExchangeBody = {
   title: 'Create Exchange',
   type: 'object',
@@ -13,16 +48,7 @@ export const createExchangeBody = {
       type: 'object',
       additionalProperties: true
     },
-    oidc4vci: {
-      title: 'OIDC4VCI Exchange options',
-      type: 'object',
-      additionalProperties: false,
-      properties: {
-        preAuthorizedCode: {
-          type: 'string'
-        }
-      }
-    }
+    oidc4vci: oidc4vciExchangeOptions
   }
 };
 
