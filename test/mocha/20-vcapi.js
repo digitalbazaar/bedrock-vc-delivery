b317d596548db666874e2a98c5a5c8d87264a18b
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-07-27 12:41:47 -0400

    Add test for `exchange w/ VC-API delivery and prc template`.

--- test/mocha/20-vcapi.js
+++ test/mocha/20-vcapi.js
@@ -7,7 +7,7 @@ import {httpClient} from '@digitalbazaar/http-client';
 import {mockData} from './mock.data.js';
 import {v4 as uuid} from 'uuid';
 
-const {credentialTemplate} = mockData;
+const {credentialTemplate, prcCredentialTemplate} = mockData;
 
 describe('exchange w/ VC-API delivery', () => {
   let capabilityAgent;
@@ -232,5 +232,107 @@ describe('exchange w/ VC-API delivery', () => {
       should.exist(err.data?.name);
       err.data.name.should.equal('NotAllowedError');
     }
+  });
+});
+
+describe('exchange w/ VC-API delivery and prc template', () => {
+  let capabilityAgent;
+  let exchangerId;
+  let exchangerRootZcap;
+  beforeEach(async () => {
+    const deps = await helpers.provisionDependencies();
+    const {
+      exchangerIssueZcap,
+      exchangerCredentialStatusZcap,
+      exchangerCreateChallengeZcap,
+      exchangerVerifyPresentationZcap
+    } = deps;
+    ({capabilityAgent} = deps);
+
+    // create exchanger instance w/ oauth2-based authz
+    const zcaps = {
+      issue: exchangerIssueZcap,
+      credentialStatus: exchangerCredentialStatusZcap,
+      createChallenge: exchangerCreateChallengeZcap,
+      verifyPresentation: exchangerVerifyPresentationZcap
+    };
+    const credentialTemplates = [{
+      type: 'jsonata',
+      template: prcCredentialTemplate
+    }];
+    const exchangerConfig = await helpers.createExchangerConfig(
+      {capabilityAgent, zcaps, credentialTemplates, oauth2: true});
+    exchangerId = exchangerConfig.id;
+    exchangerRootZcap = `urn:zcap:root:${encodeURIComponent(exchangerId)}`;
+  });
+
+  it.only('should pass', async () => {
+    // https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html
+
+    /* This flow demonstrates passing a DID Authn request and interact VC-API
+    exchange URL through CHAPI. The request is passed to a "Claimed URL"
+    which was registered on a user's device by a native app. The native app's
+    domain also published a "manifest.json" file that expressed the same
+    "Claimed URL" via `credential_handler.url='https://myapp.example/ch'` and
+    `credential_handler.launchType='redirect'` (TBD). */
+
+    const credentialId = `urn:uuid:${uuid()}`;
+    const {exchangeId} = await helpers.createCredentialOffer({
+      // local target user
+      userId: 'urn:uuid:01cc3771-7c51-47ab-a3a3-6d34b47ae3c4',
+      credentialDefinition: mockData.prcCredentialDefinition,
+      credentialId,
+      preAuthorized: true,
+      userPinRequired: false,
+      capabilityAgent,
+      exchangerId,
+      exchangerRootZcap
+    });
+    const chapiRequest = {
+      VerifiablePresentation: {
+        interact: {
+          service: [{
+            type: 'VerifiableCredentialApiExchangeService',
+            serviceEndpoint: exchangeId
+          }]
+        }
+      }
+    };
+    // CHAPI could potentially be used to deliver the URL to a native app
+    // that registered a "claimed URL" of `https://myapp.examples/ch`
+    // like so:
+    const claimedUrlFromChapi = 'https://myapp.example/ch?request=' +
+      encodeURIComponent(JSON.stringify(chapiRequest));
+    const parsedClaimedUrl = new URL(claimedUrlFromChapi);
+    const parsedChapiRequest = JSON.parse(
+      parsedClaimedUrl.searchParams.get('request'));
+
+    // post empty body and get VP w/VCs in response
+    const {
+      VerifiablePresentation: {
+        interact: {
+          service: [{serviceEndpoint: url}]
+        }
+      }
+    } = parsedChapiRequest;
+    let response;
+    let err;
+    try {
+      response = await httpClient.post(url, {agent, json: {}});
+    } catch(e) {
+      err = e;
+    }
+    console.log(err, '<><><><>err');
+    should.exist(response);
+    should.not.exist(err);
+    const {verifiablePresentation: vp} = response.data;
+    // ensure credential subject ID matches static DID
+    should.exist(vp?.verifiableCredential?.[0]?.credentialSubject?.id);
+    const {verifiableCredential: [vc]} = vp;
+    vc.credentialSubject.id.should.equal(
+      'did:example:ebfeb1f712ebc6f1c276e12ec21');
+    // ensure VC ID matches
+    should.exist(vc.id);
+    vc.id.should.equal(credentialId);
   });
 });
