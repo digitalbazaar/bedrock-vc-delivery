c9727e361d797f5eb721adeb6cfd6db3663824f5
Dave Longley <dlongley@digitalbazaar.com>
2023-10-15 18:44:05 -0400

    Add initial test for OID4VP support.

--- test/mocha/34-oid4vp.js
+++ test/mocha/34-oid4vp.js
@@ -3,12 +3,15 @@
  */
 import * as helpers from './helpers.js';
 import {agent} from '@bedrock/https-agent';
-import {httpClient} from '@digitalbazaar/http-client';
+// FIXME: remove if no longer used
+//import {httpClient} from '@digitalbazaar/http-client';
 import {mockData} from './mock.data.js';
+import {oid4vp} from '@digitalbazaar/oid4-client';
 
 const {baseUrl} = mockData;
+const {getAuthorizationRequest} = oid4vp;
 
-describe.skip('exchange w/ OID4VP + templated DID authn', () => {
+describe('exchange w/ OID4VP presentation w/DID Authn only', () => {
   let capabilityAgent;
   let exchangerId;
   let exchangerRootZcap;
@@ -34,122 +37,15 @@ describe.skip('exchange w/ OID4VP + templated DID authn', () => {
           template: `
           {
             "createChallenge": true,
-            "verifiablePresentationRequest": {
-              "query": query,
-              "domain": domain
+            "verifiablePresentationRequest": verifiablePresentationRequest,
+            "openId": {
+              "createAuthorizationRequest": "authorizationRequest",
+              "client_id_scheme": "redirect_uri",
+              "client_id": globals.exchanger.id &
+                "/exchanges/" &
+                globals.exchange.id &
+                "/client/authorization/response"
             }
-          }`
-        }
-      }
-    };
-    // set initial step
-    const initialStep = 'didAuthn';
-    const exchangerConfig = await helpers.createExchangerConfig({
-      capabilityAgent, zcaps, steps, initialStep, oauth2: true
-    });
-    exchangerId = exchangerConfig.id;
-    exchangerRootZcap = `urn:zcap:root:${encodeURIComponent(exchangerId)}`;
-  });
-
-  // FIXME: add tests that rely on VPR translation and tests that use
-  // presentation definition directly
-  it('should pass', async () => {
-    // create an exchange with appropriate variables for the step template
-    const exchange = {
-      // 15 minute expiry in seconds
-      ttl: 60 * 15,
-      // template variables
-      variables: {
-        query: {
-          type: 'DIDAuthentication',
-          acceptedMethods: [{method: 'key'}]
-        },
-        domain: 'some-random-string-that-should-work'
-      }
-    };
-    const {id: exchangeId} = await helpers.createExchange({
-      url: `${exchangerId}/exchanges`,
-      capabilityAgent, capability: exchangerRootZcap, exchange
-    });
-
-    // post to exchange URL to get expected VPR
-    let response = await httpClient.post(
-      exchangeId, {agent, json: {}});
-    should.exist(response?.data?.verifiablePresentationRequest);
-    const {data: {verifiablePresentationRequest: vpr}} = response;
-    const expectedVpr = {
-      query: {
-        type: 'DIDAuthentication',
-        acceptedMethods: [{method: 'key'}]
-      },
-      domain: exchange.variables.domain
-    };
-    expectedVpr.query.should.deep.equal(vpr.query);
-    expectedVpr.domain.should.deep.equal(vpr.domain);
-    should.exist(vpr.challenge);
-
-    // generate VP
-    const {domain, challenge} = vpr;
-    const {verifiablePresentation, did} = await helpers.createDidAuthnVP(
-      {domain, challenge});
-
-    response = await httpClient.post(
-      exchangeId, {agent, json: {verifiablePresentation}});
-    // should be no VP nor VPR in the response, indicating the end of the
-    // exchange (and nothing was issued, just presented)
-    should.not.exist(response?.data?.verifiablePresentation);
-    should.not.exist(response?.data?.verifiablePresentationRequest);
-
-    // exchange should be complete and contain the submitted VPR
-    // exchange state should be complete
-    {
-      let err;
-      try {
-        const {exchange} = await helpers.getExchange(
-          {id: exchangeId, capabilityAgent});
-        should.exist(exchange?.state);
-        exchange.state.should.equal('complete');
-        should.exist(exchange?.variables?.results?.didAuthn);
-        should.exist(
-          exchange?.variables?.results?.didAuthn?.verifiablePresentation);
-        exchange?.variables?.results?.didAuthn.did.should.equal(did);
-        exchange.variables.results.didAuthn.verifiablePresentation
-          .should.deep.equal(verifiablePresentation);
-      } catch(error) {
-        err = error;
-      }
-      should.not.exist(err);
-    }
-  });
-});
-
-describe.skip('exchange w/ OID4VP presentation + templated VPR', () => {
-  let capabilityAgent;
-  let exchangerId;
-  let exchangerRootZcap;
-  beforeEach(async () => {
-    const deps = await helpers.provisionDependencies();
-    const {
-      exchangerCreateChallengeZcap,
-      exchangerVerifyPresentationZcap
-    } = deps;
-    ({capabilityAgent} = deps);
-
-    // create exchanger instance w/ oauth2-based authz
-    const zcaps = {
-      createChallenge: exchangerCreateChallengeZcap,
-      verifyPresentation: exchangerVerifyPresentationZcap
-    };
-    // require semantically-named exchanger steps
-    const steps = {
-      // DID Authn step
-      didAuthn: {
-        stepTemplate: {
-          type: 'jsonata',
-          template: `
-          {
-            "createChallenge": true,
-            "verifiablePresentationRequest": verifiablePresentationRequest
           }`
         }
       }
@@ -173,7 +69,8 @@ describe.skip('exchange w/ OID4VP presentation + templated VPR', () => {
         verifiablePresentationRequest: {
           query: {
             type: 'DIDAuthentication',
-            acceptedMethods: [{method: 'key'}]
+            acceptedMethods: [{method: 'key'}],
+            acceptedCryptosuites: [{cryptosuite: 'Ed25519Signature2020'}]
           },
           domain: baseUrl
         }
@@ -184,21 +81,28 @@ describe.skip('exchange w/ OID4VP presentation + templated VPR', () => {
       capabilityAgent, capability: exchangerRootZcap, exchange
     });
 
-    // post to exchange URL to get expected VPR
-    let response = await httpClient.post(
-      exchangeId, {agent, json: {}});
-    should.exist(response?.data?.verifiablePresentationRequest);
-    const {data: {verifiablePresentationRequest: vpr}} = response;
-    const expectedVpr = {
-      query: {
-        type: 'DIDAuthentication',
-        acceptedMethods: [{method: 'key'}]
-      },
-      domain: baseUrl
-    };
-    expectedVpr.query.should.deep.equal(vpr.query);
-    expectedVpr.domain.should.deep.equal(vpr.domain);
-    should.exist(vpr.challenge);
+    // get authorization request
+    const authzReqUrl = `${exchangeId}/openid/client/authorization/request`;
+    const {authorizationRequest} = await getAuthorizationRequest(
+      {url: authzReqUrl, agent});
+
+    // FIXME: remove me
+    //console.log('authorizationRequest', authorizationRequest);
+
+    should.exist(authorizationRequest);
+    should.exist(authorizationRequest.presentation_definition);
+    authorizationRequest.presentation_definition.id.should.be.a('string');
+    authorizationRequest.presentation_definition.input_descriptors.should.be
+      .an('array');
+    authorizationRequest.response_mode.should.equal('direct_post');
+    authorizationRequest.nonce.should.be.a('string');
+
+    // generate VPR from authorization request
+    //const vpr = await oid4vp.toVpr({authorizationRequest});
+    // FIXME: remove me
+    //console.log('vpr', JSON.stringify(vpr, null, 2));
+
+    /*
 
     // generate VP
     const {domain, challenge} = vpr;
@@ -231,6 +135,6 @@ describe.skip('exchange w/ OID4VP presentation + templated VPR', () => {
         err = error;
       }
       should.not.exist(err);
-    }
+    }*/
   });
 });
