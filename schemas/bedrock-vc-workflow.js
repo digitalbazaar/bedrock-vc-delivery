cb6536104661caf04286341a44da21aedb2f15a7
Dave Longley <dlongley@digitalbazaar.com>
2024-07-30 13:09:07 -0400

    Add additional validation schemas for VCs, VPs, envelopes.

--- schemas/bedrock-vc-workflow.js
+++ schemas/bedrock-vc-workflow.js
@@ -4,6 +4,148 @@
 import {MAX_ISSUER_INSTANCES} from '../lib/constants.js';
 import {schemas} from '@bedrock/validation';
 
+const VC_CONTEXT_1 = 'https://www.w3.org/2018/credentials/v1';
+const VC_CONTEXT_2 = 'https://www.w3.org/ns/credentials/v2';
+
+const vcContext = {
+  oneOf: [{
+    type: 'array',
+    minItems: 1,
+    // the first context must be the VC context
+    items: [{
+      oneOf: [{
+        const: VC_CONTEXT_1
+      }, {
+        const: VC_CONTEXT_2
+      }]
+    }],
+    // additional contexts maybe strings or objects
+    additionalItems: {
+      anyOf: [{type: 'string'}, {type: 'object'}]
+    }
+  }, {
+    const: VC_CONTEXT_1
+  }, {
+    const: VC_CONTEXT_2
+  }]
+};
+
+function idOrObjectWithId() {
+  return {
+    title: 'identifier or an object with an id',
+    anyOf: [
+      schemas.identifier(),
+      {
+        type: 'object',
+        required: ['id'],
+        additionalProperties: true,
+        properties: {id: schemas.identifier()}
+      }
+    ]
+  };
+}
+
+function verifiableCredential() {
+  return {
+    title: 'Verifiable Credential',
+    type: 'object',
+    required: [
+      '@context',
+      'credentialSubject',
+      'issuer',
+      'type'
+    ],
+    additionalProperties: true,
+    properties: {
+      '@context': vcContext,
+      credentialSubject: {
+        anyOf: [
+          {type: 'object'},
+          {type: 'array', minItems: 1, items: {type: 'object'}}
+        ]
+      },
+      id: {
+        type: 'string'
+      },
+      issuer: idOrObjectWithId(),
+      type: {
+        type: 'array',
+        minItems: 1,
+        // this first type must be VerifiableCredential
+        items: [
+          {const: 'VerifiableCredential'},
+        ],
+        // additional types must be strings
+        additionalItems: {
+          type: 'string'
+        }
+      },
+      proof: schemas.proof()
+    }
+  };
+}
+
+const envelopedVerifiableCredential = {
+  title: 'Enveloped Verifiable Credential',
+  type: 'object',
+  additionalProperties: true,
+  properties: {
+    '@context': vcContext,
+    id: {
+      type: 'string'
+    },
+    type: {
+      const: 'EnvelopedVerifiableCredential'
+    }
+  },
+  required: [
+    '@context',
+    'id',
+    'type'
+  ]
+};
+
+export function verifiablePresentation() {
+  return {
+    title: 'Verifiable Presentation',
+    type: 'object',
+    required: ['@context', 'type'],
+    additionalProperties: true,
+    properties: {
+      '@context': vcContext,
+      id: {
+        type: 'string'
+      },
+      type: {
+        type: 'array',
+        minItems: 1,
+        // this first type must be VerifiablePresentation
+        items: [
+          {const: 'VerifiablePresentation'},
+        ],
+        // additional types must be strings
+        additionalItems: {
+          type: 'string'
+        }
+      },
+      verifiableCredential: {
+        anyOf: [
+          verifiableCredential(),
+          envelopedVerifiableCredential, {
+            type: 'array',
+            minItems: 1,
+            items: {
+              anyOf: [verifiableCredential(), envelopedVerifiableCredential]
+            }
+          }
+        ]
+      },
+      holder: idOrObjectWithId(),
+      proof: schemas.proof()
+    }
+  };
+}
+
 const credentialDefinition = {
   title: 'OID4VCI Verifiable Credential Definition',
   type: 'object',
@@ -327,7 +469,7 @@ export function useExchangeBody() {
     type: 'object',
     additionalProperties: false,
     properties: {
-      verifiablePresentation: schemas.verifiablePresentation()
+      verifiablePresentation: verifiablePresentation()
     }
   };
 }
