1d85ddd42e9fd578465742ea69dfb4c260d7d26f
Dave Longley <dlongley@digitalbazaar.com>
2026-05-06 14:57:53 -0400

    Support `di_vp` DID Auth proofs in OID4VCI.

--- test/mocha/31-oid4vci-did-authn.js
+++ test/mocha/31-oid4vci-did-authn.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2022-2025 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022-2026 Digital Bazaar, Inc.
  */
 import * as helpers from './helpers.js';
 import {OID4Client, parseCredentialOfferUrl} from '@digitalbazaar/oid4-client';
@@ -9,7 +9,7 @@ import {randomUUID as uuid} from 'node:crypto';
 
 const {baseUrl, didAuthnCredentialTemplate} = mockData;
 
-describe('exchange w/OID4VCI delivery + DID authn', () => {
+describe('exchange w/OID4VCI delivery + DID authn w/jwt', () => {
   let capabilityAgent;
   let workflowId;
   let workflowRootZcap;
@@ -49,6 +49,165 @@ describe('exchange w/OID4VCI delivery + DID authn', () => {
         },
         // will be used by OID4VCI
         jwtDidProofRequest: {
+          acceptedMethods: [{method: 'key'}]
+        }
+      }
+    };
+    // set initial step
+    const initialStep = 'didAuthn';
+    const workflowConfig = await helpers.createWorkflowConfig({
+      capabilityAgent, zcaps, credentialTemplates, steps, initialStep,
+      oauth2: true
+    });
+    workflowId = workflowConfig.id;
+    workflowRootZcap = `urn:zcap:root:${encodeURIComponent(workflowId)}`;
+  });
+
+  it('should pass w/ pre-authorized code flow', async () => {
+    // pre-authorized flow, issuer-initiated
+    const credentialId = `urn:uuid:${uuid()}`;
+    const {offerUrl} = await helpers.createCredentialOffer({
+      // local target user
+      userId: 'urn:uuid:01cc3771-7c51-47ab-a3a3-6d34b47ae3c4',
+      credentialDefinition: mockData.credentialDefinition,
+      credentialId,
+      preAuthorized: true,
+      userPinRequired: false,
+      capabilityAgent,
+      workflowId,
+      workflowRootZcap
+      // FIXME: add test with a `requiredDid` -- any presented VPs must include
+      // DID Authn with a DID that matches the `requiredDid` value -- however,
+      // this might be generalized into some other kind of VPR satisfaction
+      // mechanism
+    });
+    const chapiRequest = {OID4VCI: offerUrl};
+    // CHAPI could potentially be used to deliver the URL to a native app
+    // that registered a "claimed URL" of `https://myapp.examples/ch`
+    // like so:
+    const claimedUrlFromChapi = 'https://myapp.example/ch?request=' +
+      encodeURIComponent(JSON.stringify(chapiRequest));
+    const parsedClaimedUrl = new URL(claimedUrlFromChapi);
+    const parsedChapiRequest = JSON.parse(
+      parsedClaimedUrl.searchParams.get('request'));
+    const offer = parseCredentialOfferUrl({url: parsedChapiRequest.OID4VCI});
+
+    // wallet / client gets access token
+    const client = await OID4Client.fromCredentialOffer({offer, agent});
+
+    const {did, signer: didProofSigner} = await helpers.createDidProofSigner();
+
+    // wallet / client receives credential
+    const result = await client.requestCredential({
+      credentialDefinition: mockData.credentialDefinition,
+      did,
+      didProofSigner,
+      agent
+    });
+    should.exist(result);
+    result.should.include.keys(['format', 'credential']);
+    result.format.should.equal('ldp_vc');
+    // ensure credential subject ID matches generated DID
+    should.exist(result.credential?.credentialSubject?.id);
+    result.credential.credentialSubject.id.should.equal(did);
+    // ensure VC ID matches
+    should.exist(result.credential.id);
+    result.credential.id.should.equal(credentialId);
+  });
+
+  it('should pass w/ fetched nonce', async () => {
+    // pre-authorized flow, issuer-initiated
+    const credentialId = `urn:uuid:${uuid()}`;
+    const {offerUrl} = await helpers.createCredentialOffer({
+      // local target user
+      userId: 'urn:uuid:01cc3771-7c51-47ab-a3a3-6d34b47ae3c4',
+      credentialDefinition: mockData.credentialDefinition,
+      credentialId,
+      preAuthorized: true,
+      userPinRequired: false,
+      capabilityAgent,
+      workflowId,
+      workflowRootZcap
+    });
+    const chapiRequest = {OID4VCI: offerUrl};
+    // CHAPI could potentially be used to deliver the URL to a native app
+    // that registered a "claimed URL" of `https://myapp.examples/ch`
+    // like so:
+    const claimedUrlFromChapi = 'https://myapp.example/ch?request=' +
+      encodeURIComponent(JSON.stringify(chapiRequest));
+    const parsedClaimedUrl = new URL(claimedUrlFromChapi);
+    const parsedChapiRequest = JSON.parse(
+      parsedClaimedUrl.searchParams.get('request'));
+    const offer = parseCredentialOfferUrl({url: parsedChapiRequest.OID4VCI});
+
+    // wallet / client gets access token
+    const client = await OID4Client.fromCredentialOffer({offer, agent});
+
+    const {did, signer: didProofSigner} = await helpers.createDidProofSigner();
+
+    // get nonce from server
+    const {nonce} = await client.getNonce({agent});
+
+    // wallet / client receives credential
+    const result = await client.requestCredential({
+      credentialDefinition: mockData.credentialDefinition,
+      nonce,
+      did,
+      didProofSigner,
+      agent
+    });
+    should.exist(result);
+    result.should.include.keys(['format', 'credential']);
+    result.format.should.equal('ldp_vc');
+    // ensure credential subject ID matches generated DID
+    should.exist(result.credential?.credentialSubject?.id);
+    result.credential.credentialSubject.id.should.equal(did);
+    // ensure VC ID matches
+    should.exist(result.credential.id);
+    result.credential.id.should.equal(credentialId);
+  });
+});
+
+describe('exchange w/OID4VCI delivery + DID authn w/di_vp', () => {
+  let capabilityAgent;
+  let workflowId;
+  let workflowRootZcap;
+  beforeEach(async () => {
+    const deps = await helpers.provisionDependencies();
+    const {
+      workflowIssueZcap,
+      workflowCredentialStatusZcap,
+      workflowCreateChallengeZcap,
+      workflowVerifyPresentationZcap
+    } = deps;
+    ({capabilityAgent} = deps);
+
+    // create workflow instance w/ oauth2-based authz
+    const zcaps = {
+      issue: workflowIssueZcap,
+      credentialStatus: workflowCredentialStatusZcap,
+      createChallenge: workflowCreateChallengeZcap,
+      verifyPresentation: workflowVerifyPresentationZcap
+    };
+    const credentialTemplates = [{
+      type: 'jsonata',
+      template: didAuthnCredentialTemplate
+    }];
+    // require semantically-named workflow steps
+    const steps = {
+      // DID Authn step
+      didAuthn: {
+        createChallenge: true,
+        // will be used by VC-API
+        verifiablePresentationRequest: {
+          query: {
+            type: 'DIDAuthentication',
+            acceptedMethods: [{method: 'key'}]
+          },
+          domain: baseUrl
+        },
+        // will be used by OID4VCI
+        divpDidProofRequest: {
           acceptedMethods: [{method: 'key'}]
         }
       }
